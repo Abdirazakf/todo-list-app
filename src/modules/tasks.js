424b4c3a9a2c7893c92828bf7d54cd341e745f25
Abdirazakf <farahabdirazak13@gmail.com>
2025-07-21 23:22:46 -0400

    Added the all tasks page with all tasks from all projects being displayed

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -49,7 +49,7 @@ export default class Tasks {
             const dueDate = formData.get("date")
             const priority = formData.get("priority")
             const project = formData.get("project")
-            
+
             if (title && project){
                 const newTask = {
                     id: "t" + crypto.randomUUID(),
@@ -84,8 +84,70 @@ export default class Tasks {
         }
     }
 
+    allTasks() {
+        const button = document.querySelector(".all-tasks-button")
+        const content = document.querySelector(".content")
+
+        button.addEventListener("click", () =>{
+            content.innerHTML = ""
+            const header = document.createElement("h1")
+            header.textContent = "All Tasks"
+            const contentArea = document.createElement("div")
+            contentArea.classList.add("project-task-area")
+
+            this.projects.projects.forEach(project =>{
+                project.tasks.forEach(task => {
+                    const taskCard = document.createElement("div")
+                    taskCard.classList.add("card")
+                    const cardContent = document.createElement("div")
+                    cardContent.classList.add("card-content")
+                    
+                    const taskTitle = document.createElement("h3")
+                    taskTitle.textContent = task.title
+                    cardContent.appendChild(taskTitle)
+                
+                    const taskDesc = document.createElement("p")
+                    taskDesc.textContent = `Description: ${task.desc}`
+                    cardContent.appendChild(taskDesc)
+                
+                    const taskDate = document.createElement("p")
+                    taskDate.textContent = `Due Date: ${task.dueDate}`
+                    cardContent.appendChild(taskDate)
+                    
+                    const taskPriority = document.createElement("p")
+                    taskPriority.textContent = `Priority: ${task.priority}`
+                    cardContent.appendChild(taskPriority)
+                    
+                    const statusDiv = document.createElement("div")
+                    const taskStatus = document.createElement("input")
+                    const statusLabel = document.createElement("label")
+                    statusLabel.textContent = "Completed"
+                    statusLabel.htmlFor = "completed"
+                    taskStatus.setAttribute("id", "completed")
+                    taskStatus.type = "checkbox"
+                    taskStatus.checked = task.completed || false
+                    
+                    taskStatus.addEventListener("change", () =>{
+                        projectClass.toggleTaskComplete(project.id, task.id)
+                        console.log(project)
+                    })
+                
+                    statusDiv.appendChild(statusLabel)
+                    statusDiv.appendChild(taskStatus)
+                    cardContent.appendChild(statusDiv)
+                    
+                    taskCard.appendChild(cardContent)
+                    contentArea.appendChild(taskCard)
+                })
+                content.appendChild(header)
+                content.appendChild(contentArea)
+            })
+        })
+    }      
+
     init() {
         this.tasksUIEvents()
         this.handleTaskForm()
+        this.allTasks()
     }
 }
